9ba3bc2148647598ade2b74f519e0e368cdf204e
valeskaperezrretamal <jperezr@udd.cl>
2019-04-25 10:45:38 -0400

    se comienza a modificar función mdLinks, para pasarla a asincrona con promesas, se le pasa a files la función que se creará ReadPath, se agrega then para que ejecute la función getlinks y se agrega catch en caso de error, que muestre reason, error de la repecificación de promesas

--- mdLinks.js
+++ mdLinks.js
@@ -1,36 +1,17 @@
 const fs = require('fs'); //para poder ocupar la libreria,sistema de archivos
-const myMarked = require('marked');//renderizar un texto plno a otro como html u otros
+const myMarked = require('marked');//renderizar un texto plano a otro como html u otros
 const libpath = require("path");//manjear rutas, aquí esta join
 
-
+//se agrega a files ReadPath
 const mdLinks = (path,options) =>{
-    let files =[];
-    let stats = fs.statSync(path);//lee información del archivo
-
-    if (stats.isDirectory()){
-        files = fs.readdirSync(path);//guard archivos del dirctorio
-        for(let i=0; i<files.length;i++){files[i]=libpath.join(path, files[i]);};//me duvuelve un string,concatena dos rutas, archivo y carpeta, genera la ruta del archivo 
-        files=files.filter((file)=>{return getFileExtension3(file)==="md"});//devuelve archivos con extención .md
-        
-    } else if(stats.isFile() && getFileExtension3(path)==="md"){//ingresa un archivo y debe ser con extención .ms
-        files[0]=path;
-
-    }else return console.log("ingresa un archivo .md o un directorio con archivos .md");
-    if (files.length<1) return console.log("ingresa un archivo .md o un directorio con archivos .md")
-    
-    let links=[];
-    let markdown="";
-    files.forEach(file=>{
-         markdown = fs.readFileSync(file).toString(); //lee archivos markdown y los combierte en string
-         links= links.concat(getLinksFromMd(markdown,file));
-    });
-    
+    let files= ReadPath(path);
+    let links=files.then(getlinks)
+    .catch(reason=>{console.log(reason)});
     return links;
-      
-};
-module.exports=mdLinks;
+    };
+    module.exports=mdLinks;
 
-// me devuelve array con links 
+    // me devuelve array con links 
 const getLinksFromMd= (markdown,path)=>{
     const renderer = new myMarked.Renderer();
     const output=[];
@@ -46,10 +27,37 @@ const getLinksFromMd= (markdown,path)=>{
     return output;
 
 
-};
+}
 
+    
 // fuente http://www.jstips.co/es_es/javascript/get-file-extension/
 function getFileExtension3(filename) {
     return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
-  };
+  }
+
+    if (stats.isDirectory()){
+        files = fs.readdirSync(path);//guard archivos del dirctorio
+        for(let i=0; i<files.length;i++){files[i]=libpath.join(path, files[i]);};//me duvuelve un string,concatena dos rutas, archivo y carpeta, genera la ruta del archivo 
+        files=files.filter((file)=>{return getFileExtension3(file)==="md"});//devuelve archivos con extención .md
+        
+    } else if(stats.isFile() && getFileExtension3(path)==="md"){//ingresa un archivo y debe ser con extención .md
+        files[0]=path;
+
+    }else return console.log("ingresa un archivo .md o un directorio con archivos .md");
+    if (files.length<1) return console.log("ingresa un archivo .md o un directorio con archivos .md")
+    
+    let links=[];
+    let markdown="";
+    files.forEach(file=>{
+         markdown = fs.readFileSync(file).toString(); //lee archivos markdown y los combierte en string
+         links= links.concat(getLinksFromMd(markdown,file));
+    });
+    
+    return links;
+      
+}
+    )};
+
+
+
 
