e4332a07eedaeac031c8984dc8b3efb36edcf64d
valeskaperezrretamal <jperezr@udd.cl>
2019-04-26 10:58:25 -0400

    se agrega función convertir F.S a promesa para leer directorio

--- mdLinks.js
+++ mdLinks.js
@@ -54,24 +54,36 @@ const stat=(path)=>{
       });
     });
   }
+  function readDir(path) {
+    return new Promise((resolve, reject) => {
+      fs.readdir(path, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
 
-
-
-//input: ruta  output: array con ruta de archivos MD
+//input: ruta  
+//output: array con ruta de archivos MD
 const ReadPath= (path)=>{
     let files = stat(path).then((stats)=>{
-         if (stats.isDirectory()){
-        return Promise.all(readDir(path).filter((file)=>{
-            return getFileExtension3(file)==="md";
-        })).then((result)=>{
-            output=[];
-            for(let i=0; i<result.length;i++){output[i]=libpath.join(path, result[i]);}
-                if (output.length<1){throw "elija un archivo o directorio con archivos .md";}
+        if (stats.isDirectory()){
+           return (readDir(path))
+          .then((files)=>{
+             files.filter((file)=>{return getFileExtension3(file)==="md";});
+              files=files.map((file)=>{
+                return libpath.join(path,file);
+              })
+              if (files.length<1){throw "elija un archivo o directorio con archivos .md";}
+              return files;
+          });
+         
+        } else if (stats.isFile() && getFileExtension3(path)==="md" ){
+            let output=[];
+            output[0]=path
             return output;
-        });
-
-        } else if (stats.isDirectory() && getFileExtension3(path)==="md" ){
-            return [path];
         } else {throw "elija un archivo o directorio valido";}
     });
     return files;    
