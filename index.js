19ef9908d8c3acc9789c69b598192618747c800c
valeskaperezrretamal <jperezr@udd.cl>
2019-04-26 11:34:17 -0400

    se arregla el llamado a la función y se mantiene la importación de función mdLinks

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
-#!/usr/bin/env node
-
+#!/usr/bin/env node  
 //me avisa que se va a ejecutar con node
 
-//module.exports = () => {
-  // ...
-//};
-//ejemplos
-//const math = require (' ./src/math');
 
-//console.log(math.add(2,2))
-//console.log(math.multiply)
-//const fs= require('fs');
-
-//fs.readFile('.src')
-//const fs = require('fs');
-
-//fs.readFile('readme.md','utf-8', (err, data) => { 
-  //if (err) throw err;
-  //console.log(data);
-
-//});
-
-//Acá llamo a la función
-
+//se importa función
 let mdLinks= require('./mdLinks.js');
+//Acá llamo a la función
+mdLinks('./mdsparaPruebas').then(links=>{
+  console.log(links);
+});
 
-let links = mdLinks('./mdsparaPruebas');
-
- console.log(links);
-
+ //forma de llamado
  //console.log(process.argv[2]);
  //console.log(process.argv[3]);
 
-
-//se puede gardar un promesa en una variable
-//myPrommiseArray = [];
-//PRO TIO  =let myPromise =mdlinks ("asadffd");
-//myPromiseArray.push(myPromise);
-//...mucho codigo despues...Promise.all(myPromiseArray);
-//allPromises.then((allAnsewers)=>{ })
-
-//sumar los elementos de un arreglo de forma recuersiva
-//const addNumersInArray = (arr)=>{}
-//tarea hacer una función ue retorne un arreglo invertido
-//arreglo de string o numeroes invertirlo
 
 
 
