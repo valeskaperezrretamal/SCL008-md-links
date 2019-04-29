660a9023d733540a4e904f14254233fb1bbd11d3
valeskaperezrretamal <jperezr@udd.cl>
2019-04-28 21:51:46 -0400

    se agrego process.argv

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-#!/usr/bin/env node  
-//me avisa que se va a ejecutar con node
+ 
+
 
 
 //se importa función
 let mdLinks= require('./mdLinks.js');
 //Acá llamo a la función
-mdLinks('./mdsparaPruebas').then(links=>{
+mdLinks(process.argv[2]).then(links=>{
   console.log(links);
 });
 
  //forma de llamado
  //console.log(process.argv[2]);
  //console.log(process.argv[3]);
-
 
 
 
