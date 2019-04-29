922f70369aca83cf2df2a87e4a20301994e13677
valeskaperezrretamal <jperezr@udd.cl>
2019-04-28 21:53:26 -0400

    se optimiza codigo, se utiliza fs.promises

--- mdLinks.js
+++ mdLinks.js
@@ -1,4 +1,6 @@
-const fs = require('fs'); //para poder ocupar la libreria,sistema de archivos
+#!/usr/bin/env node
+
+const fsPromises = require('fs').promises; //para poder ocupar la libreria,sistema de archivos
 const myMarked = require('marked');//renderizar un texto plano a otro como html u otros
 const libpath = require("path");//manjear rutas, aquí esta join
 
@@ -6,8 +8,7 @@ const libpath = require("path");//manjear rutas, aquí esta join
 const mdLinks = (path,options) =>{
     return ReadPath(path)
     .then(getlinks)
-    .catch(reason=>{console.log("error: " + reason)});
-    
+    .catch(console.log);    
     };
 
 module.exports=mdLinks;
@@ -33,60 +34,33 @@ function getFileExtension3(filename) {
     return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
   }
 
-  //convierte funciones FS a promesas, fuente https://www.frankmitchell.org/2017/06/promise-objects/
-const stat=(path)=>{
-    return new Promise((resolve) => {
-      fs.stat(path, (err, result) => {
-        if (err) {
-          result = {size: 0, mtime: Date.now()};
-        }
-        resolve(result);
-      });
-    });
-  }
-  function readFile(path) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(path, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(data);
-      });
-    });
-  }
-  function readDir(path) {
-    return new Promise((resolve, reject) => {
-      fs.readdir(path, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(data);
-      });
-    });
-  }
-
   //input: ruta  
 //output: array con ruta de archivos MD
 const ReadPath= (path)=>{
-    let files = stat(path).then((stats)=>{
+  return fsPromises.stat(path)
+    .then((stats)=>{
         if (stats.isDirectory()){
-           return (readDir(path))
+          return (fsPromises.readdir(path))
           .then((files)=>{
-             files.filter((file)=>{return getFileExtension3(file)==="md";});
-              files=files.map((file)=>{
+             let filteredfiles=files.filter((file)=>{return getFileExtension3(file)==="md";});
+             let mapedFiles=filteredfiles.map((file)=>{
                 return libpath.join(path,file);
-              })
-              if (files.length<1){throw "elija un archivo o directorio con archivos .md";}
-              return files;
-          });
+              });
+              if (mapedFiles.length<1){throw "elija un archivo o directorio con archivos .md";}
+              return mapedFiles;
+          })
+         
          
         } else if (stats.isFile() && getFileExtension3(path)==="md" ){
             let output=[];
             output[0]=path
             return output;
         } else {throw "elija un archivo o directorio valido";}
-    });
-    return files;    
+    })
+    .catch(reason=>{
+      if(reason.code==='ENOENT'){//capturar el error de objeto de node fs.stat
+        throw "Error: ingrese una ruta valida";
+      }});        
 }  
 // input: ruta de archivo .md 
 // output: promesa, array de objetos con propiedades Href, Text y File
@@ -96,7 +70,7 @@ const getlinks = (files)=>{
     let promisesArray=[];
     let links=[]
     files.forEach(file=>{        
-      promisesArray.push(readFile(file));
+      promisesArray.push(fsPromises.readFile(file));
       
       });
 
